eaa178972bb459bca1f5d25c92a4d1d929883f89
Samir Chaudhary <sc5456216@gmail.com>
2026-08-02 04:07:51 +0000

    Add missing cache_page import

--- student_guidance_system/urls.py
+++ student_guidance_system/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.views.decorators.cache import cache_page
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenRefreshView
